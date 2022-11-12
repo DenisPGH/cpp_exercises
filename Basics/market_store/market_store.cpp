d5b9c0933c69dca80058287265c7ebab9e4ea84c
Denislav <deni_9000_1990@abv.bg>
2022-11-12 11:18:10 +0100

    task 1

---
+++ Basics/market_store/market_store.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double neededMoney;
+    double priceErdbeeren; cin >> priceErdbeeren;
+    double quantiteBannansKG; cin >> quantiteBannansKG;
+    double quantiteOrangeKG; cin >> quantiteOrangeKG;
+    double quantiteMaliniKG; cin >> quantiteMaliniKG;
+    double quantiteErdbeereKG; cin >> quantiteErdbeereKG;
+
+    double priceMalini = priceErdbeeren * 0.5;
+    double priceOrange = priceMalini * 0.6;
+    double priceBananas = priceMalini * 0.2;
+
+    neededMoney = priceErdbeeren * quantiteErdbeereKG + priceBananas * quantiteBannansKG +
+        priceMalini * quantiteMaliniKG + priceOrange * quantiteOrangeKG;
+
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    std::cout << neededMoney;
+    
+}
+

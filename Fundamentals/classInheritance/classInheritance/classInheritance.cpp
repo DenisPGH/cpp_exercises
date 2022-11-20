86e432a614cdc59d5553ad4b823e3f8f4b815b22
Denislav <deni_9000_1990@abv.bg>
2022-11-20 12:44:51 +0100

    inheritance and abstract to other class

--- Fundamentals/classInheritance/classInheritance/classInheritance.cpp
+++ Fundamentals/classInheritance/classInheritance/classInheritance.cpp
@@ -7,13 +7,15 @@ using namespace std;
 class Person {
     int Age=0;
     string Name=" no name";
+   
+public:
     double height;
     double weight;
-public:
-    Person(double height, double weight) : height(height), weight(weight) {};
+    Person(double height, double weight) : height{ height }, weight{ weight } {};
+    Person() { height = 0, weight = 0.0; };
     ~Person() { cout << "destructor"; };
 
-    void getBody(double a, string n) {
+    void getBody(int a, string n) {
         Age = a;
         Name = n;
     }
@@ -25,10 +27,11 @@ public:
         return res;
 
     }
+    // implicit
+   /* Person(const Person&) = delete;
+    Person& operator=(const Person&) = delete;*/
 private:
     string sex = "male";
-
-
 
 };
 
@@ -36,17 +39,54 @@ void print_function(string toPrint) {
     cout << " person info:  " << toPrint << endl;
 }
 
+class Miro {
+public:
+    int years;
+    
+    Miro() { years = 10; }
+    double db() {
+        return years2 * years2;
+    }
+private:
+    int years2 = 20;
+   
+};
+
+
+
+class Denis : public Person {
+public:
+    Miro miro;
+    
+   /* Denis(double h, double w) {
+        height = h;
+        weight = w;
+    };*/
+    //Denis() {};
+
+};
+
 int main()
 {
     Person deni(180.0,80.0);
-
-    cout.precision(2);
     print_function(deni.info());
-    int age; cin >> age;
+    /*int age; cin >> age;
     string name; cin >> name;
     deni.getBody(age, name);
-    //cout << " person info after:  " << deni.info() << endl;
+    print_function(deni.info());*/
+
+    Denis r;
+    r.getBody(30, "Denis@");
+    print_function(r.info());
+    r.height = 300;
+    r.weight = 50;
+    print_function(r.info());
     print_function(deni.info());
+
+    cout << " reference to miro " + to_string(r.miro.years)<< endl;
+    cout << " reference to miro " + to_string(r.miro.db())<< endl;
+ 
+
     return 0;
 }
 

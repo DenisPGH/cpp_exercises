608ede014e1ff0e5b7712c0b173e32c986f12f7b
Denislav <deni_9000_1990@abv.bg>
2022-12-04 12:00:57 +0100

    most freq number

--- Fundamentals/most_freq_number/most_freq_number/most_freq_number.cpp
+++ Fundamentals/most_freq_number/most_freq_number/most_freq_number.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
+
 using namespace std;
 
 int* store_arr(int arr[], int len) {
@@ -31,13 +34,61 @@ int* read_number(int arr[],int size){
 }
 
 int main()
-{
+{  
+    // read and store into array
     int size_arr; cin >> size_arr;
     int* arr = new int[size_arr];
     for (int i = 0; i < size_arr; ++i) {
         cin >> arr[i];
         //cout << " num = " << arr[i];
     }
+    // create new unordered map
+    unordered_map<int, int> number_count;
+
+    // store numbers and values
+    for (int i = 0; i < size_arr; ++i) {
+        number_count[arr[i]] += 1;
+        
+    }
+
+    // find most one
+    vector<int> most_numbers;
+    int most_num_value = 0;
+    int most_num_key = 0;
+    for (auto& y : number_count) {
+        if (y.second >= most_num_value) {
+            most_num_value = y.second; 
+            most_num_key = y.first;
+            most_numbers.push_back(y.first);
+            
+        }
+
+
+        //cout << "key: " << y.first << " value: " << y.second << endl;
+    }
+    
+
+
+    /*cout << "The number " << most_num_key << " is the most frequent (occurs " << most_num_value << " times)"<< endl;*/
+    
+    // sort the result and print
+    /*for (int& x : most_numbers) {
+        if (x < most_num_value) { most_numbers.erase(x); }
+    }*/
+    sort(most_numbers.begin(), most_numbers.end());
+
+    
+    for (auto& k : most_numbers) {
+        cout << " all most: " << k<< endl;
+        if (number_count[k] == most_num_value) {
+            cout << k << " ";
+        }
+ 
+    }
+    
+
+
+
    
     //cout << "size of array = " << size_arr<< endl;
     //int* res=store_arr(arr, size_arr);
@@ -46,7 +97,7 @@ int main()
     
 
     //cout << "array ist = " << *res << endl;
-    print_arr(arr,size_arr);
+    //print_arr(arr,size_arr);
 
     
     
